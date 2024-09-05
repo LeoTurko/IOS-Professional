4fbffb1cbbc7ab715e366454445e6f6e0d4c0464
Leo <leo-turko@mail.ru>
2024-09-05 21:38:39 +0300

    feat: Shake sign in button if login false

--- Bankey/Bankey/LoginViewController.swift
+++ Bankey/Bankey/LoginViewController.swift
@@ -145,7 +145,7 @@ extension LoginViewController {
       return
     }
     
-    guard !username.isEmpty, !password.isEmpty else {
+    if username.isEmpty || password.isEmpty {
       configureView(withMessage: "Username / password connot be blank")
       return
     }
@@ -162,6 +162,17 @@ extension LoginViewController {
   private func configureView(withMessage message: String) {
     errorMessageLabel.isHidden = false
     errorMessageLabel.text = message
+    shakeButton()
+  }
+  private func shakeButton() {
+    let animation = CAKeyframeAnimation()
+    animation.keyPath = "position.x"
+    animation.values = [0, 10, -10, 10, 0]
+    animation.keyTimes = [0, 0.16, 0.5, 0.83, 1]
+    animation.duration = 0.4
+    
+    animation.isAdditive = true
+    signInButton.layer.add(animation, forKey: "shake")
   }
 }
 
