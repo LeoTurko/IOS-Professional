898f63dba4481e129dbf64c343f7b28094e93ba7
Leo <leo-turko@mail.ru>
2024-09-21 23:30:05 +0300

    feat: Group account summary network calls together

--- Bankey/Bankey/AccountSummaryViewController.swift
+++ Bankey/Bankey/AccountSummaryViewController.swift
@@ -29,12 +29,7 @@ class AccountSummaryViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     setup()
-    setupNavigationBar()
-    fetchDataAndLoadViews()
-  }
-  
-  func setupNavigationBar() {
-    navigationItem.rightBarButtonItem = logoutBarButtonItem
+    fetchData()
   }
 }
 
@@ -43,6 +38,7 @@ extension AccountSummaryViewController {
     view.backgroundColor = .white
     setupTableView()
     setupTableHeaderView()
+    setupNavigationBar()
   }
   
   private func setupTableView() {
@@ -74,6 +70,10 @@ extension AccountSummaryViewController {
     headerView.frame.size = size
     
     tableView.tableHeaderView = headerView
+  }
+  
+  func setupNavigationBar() {
+    navigationItem.rightBarButtonItem = logoutBarButtonItem
   }
 }
 
@@ -110,27 +110,35 @@ extension AccountSummaryViewController {
 
 // MARK: - Networking
 extension AccountSummaryViewController {
-  private func fetchDataAndLoadViews() {
+  private func fetchData() {
+    let group = DispatchGroup()
+    
+    group.enter()
     fetchProfile(forUserId: "1") { result in
       switch result {
       case .success(let profile):
         self.profile = profile
         self.configureTableHeaderView(with: profile)
-        self.tableView.reloadData()
       case .failure(let error):
         print(error.localizedDescription)
       }
+      group.leave()
     }
     
+    group.enter()
     fetchAccounts(forUserId: "1") { result in
       switch result {
       case .success(let accounts):
         self.accounts = accounts
         self.configureTableCells(with: accounts)
-        self.tableView.reloadData()
       case .failure(let error):
         print(error.localizedDescription)
       }
+      group.leave()
+    }
+    
+    group.notify(queue: .main) {
+      self.tableView.reloadData()
     }
   }
   
